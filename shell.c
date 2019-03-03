1a539f2d7421f608bebd3f1ef26848d3dac34f7d
anandthegreat <anand17218@iiitd.ac.in>
2019-03-03 19:14:36 +0530

    append (>>) redirection done

--- shell.c
+++ shell.c
@@ -19,31 +19,37 @@ void tokeniser(char *input,char **args){
 int redirectionChecker(char *input,char **args){
 
   char *p=strstr(input,">");                                //check if > exists in input string
+  char *q=strstr(input,">>");                               //check if >> exists in input string
 //   int i=0;
 //   args[i] = strtok(input,">");                           //arr[0] will have command name
 //   while(args[i]!=NULL) args[++i] = strtok(NULL,">");
 // printf("%d\n", i);
+
+  
+  if(q!=NULL) return 2; 
   if(p!=NULL) return 1;
   else return 0;
 //
 //   args[++i]=NULL;
 
 }
-void redirectionExecution(char *input,char *filename,char **args){
+void redirectionExecution(char *input,char *filename,char **args,int append_or_truncate){
   printf("====>%s\n",filename );
 
-  if(filename[0]=='>') //for append not working
+  if(append_or_truncate==2)             // >> case
+//  if(filename[0]=='>') //for append not working
   {
-
+    printf("%s\n",">> case" );
     close(1);
-    int fd = open(filename,O_WRONLY|O_APPEND,0777);
+    int fd = open(filename,O_CREAT|O_WRONLY|O_APPEND,0777);
     tokeniser(input,args);
     if(execvp(args[0],args)<0){
       perror("error: exec failed");
       exit(1);
     }
   }
-  else{
+  else if(append_or_truncate==1){     // > case
+
     // printf("%d\n",filename[0]=='>');
     close(1);
     int fd = open(filename,O_WRONLY|O_CREAT|O_TRUNC,0777);
@@ -61,7 +67,8 @@ void commandExecution(char input[])
     if (pID==0){
       signal(SIGINT, SIG_DFL);	
       char *args[20];
-      if(redirectionChecker(input, args)==0)
+      int redirectionType=redirectionChecker(input, args);
+      if(redirectionType==0)
       {
         tokeniser(input,args);
         if(execvp(args[0],args)<0){                   //execute command and check if unsuccessful
@@ -69,11 +76,20 @@ void commandExecution(char input[])
           exit(1);
         }
       }
-      else{
+      else if(redirectionType==1){                   // >
         char *filename;
         strtok(input,">");
         filename= strtok(NULL," ");
-        redirectionExecution(input,filename,args);
+        redirectionExecution(input,filename,args,1);
+      }
+      else if(redirectionType==2){                  // >>
+        char *filename;
+        char *ptr = input;
+        filename=strtok_r(ptr,">>",&ptr);
+        filename=strtok_r(ptr,">>",&ptr);
+        filename=strtok(filename," ");
+        printf("%s\n",filename);
+        redirectionExecution(input,filename,args,2);
       }
     }
     else if(pID>0){
