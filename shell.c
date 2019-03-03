a6c495e388e61182d1ef633c05b229dfe60c55ce
anandthegreat <anand17218@iiitd.ac.in>
2019-03-03 20:54:27 +0530

    input redirection < done

--- shell.c
+++ shell.c
@@ -7,6 +7,10 @@
 #include<fcntl.h>
 #include<signal.h>
 
+enum type { 
+  OVERWRITE2FILE, APPEND2FILE, READFROMFILE, NORMAL 
+} Type;
+
 void dontKillMe();											//function prototypes
 
 void tokeniser(char *input,char **args){
@@ -16,49 +20,78 @@ void tokeniser(char *input,char **args){
   args[++i]=NULL;
 }
 
-int redirectionChecker(char *input,char **args){
+enum type redirectionChecker(char *input,char **args){
 
   char *p=strstr(input,">");                                //check if > exists in input string
   char *q=strstr(input,">>");                               //check if >> exists in input string
+  char *r=strstr(input,"<");
 //   int i=0;
 //   args[i] = strtok(input,">");                           //arr[0] will have command name
 //   while(args[i]!=NULL) args[++i] = strtok(NULL,">");
 // printf("%d\n", i);
 
   
-  if(q!=NULL) return 2; 
-  if(p!=NULL) return 1;
-  else return 0;
+  if(q!=NULL)      return APPEND2FILE; //should be above p!=NULL check
+  else if(p!=NULL) return OVERWRITE2FILE;
+  else if(r!=NULL) return READFROMFILE;
+  else return NORMAL;
 //
 //   args[++i]=NULL;
 
 }
-void redirectionExecution(char *input,char *filename,char **args,int append_or_truncate){
+void redirectionExecution(char *input,char *filename,char **args,enum type r_type){ //r_type = redirection type
   printf("====>%s\n",filename );
 
-  if(append_or_truncate==2)             // >> case
+  if(r_type==APPEND2FILE)             // >> case
 //  if(filename[0]=='>') //for append not working
   {
-    printf("%s\n",">> case" );
+    //printf("%s\n",">> case" );
     close(1);
-    int fd = open(filename,O_CREAT|O_WRONLY|O_APPEND,0777);
+    int fd;
+    if(fd= open(filename,O_CREAT|O_WRONLY|O_APPEND,0777)==-1){
+      perror("failed to open/create file");
+    }
+    else{
     tokeniser(input,args);
     if(execvp(args[0],args)<0){
       perror("error: exec failed");
       exit(1);
+      }
     }
   }
-  else if(append_or_truncate==1){     // > case
+
+  else if(r_type==OVERWRITE2FILE){     // > case
 
     // printf("%d\n",filename[0]=='>');
     close(1);
-    int fd = open(filename,O_WRONLY|O_CREAT|O_TRUNC,0777);
+    int fd;
+    if(fd = open(filename,O_WRONLY|O_CREAT|O_TRUNC,0777)==-1){
+      perror("failed to open/create file");
+    }
+    else{
     tokeniser(input,args);
     if(execvp(args[0],args)<0){
       perror("error: exec failed");
       exit(1);
+      }
     }
   }
+
+  else if(r_type==READFROMFILE){
+    close(0);
+    int fd;
+    if(fd= open(filename,O_RDONLY)==-1){
+      perror("failed to open file");
+    } 
+    else{
+    tokeniser(input,args);
+    if(execvp(args[0],args)<0){
+      perror("error: exec failed");
+      exit(1);
+      }
+    }
+  }
+
 }
 
 void commandExecution(char input[])
@@ -67,8 +100,8 @@ void commandExecution(char input[])
     if (pID==0){
       signal(SIGINT, SIG_DFL);	
       char *args[20];
-      int redirectionType=redirectionChecker(input, args);
-      if(redirectionType==0)
+      Type=redirectionChecker(input, args);
+      if(Type==NORMAL)
       {
         tokeniser(input,args);
         if(execvp(args[0],args)<0){                   //execute command and check if unsuccessful
@@ -76,20 +109,26 @@ void commandExecution(char input[])
           exit(1);
         }
       }
-      else if(redirectionType==1){                   // >
+      else if(Type==OVERWRITE2FILE){                   // >
         char *filename;
         strtok(input,">");
         filename= strtok(NULL," ");
-        redirectionExecution(input,filename,args,1);
+        redirectionExecution(input,filename,args,OVERWRITE2FILE);
       }
-      else if(redirectionType==2){                  // >>
+      else if(Type==APPEND2FILE){                  // >>
         char *filename;
         char *ptr = input;
         filename=strtok_r(ptr,">>",&ptr);
         filename=strtok_r(ptr,">>",&ptr);
         filename=strtok(filename," ");
         printf("%s\n",filename);
-        redirectionExecution(input,filename,args,2);
+        redirectionExecution(input,filename,args,APPEND2FILE);
+      }
+      else if(Type==READFROMFILE){
+        char *filename;
+        strtok(input,"<");
+        filename= strtok(NULL," ");
+        redirectionExecution(input,filename,args,READFROMFILE);
       }
     }
     else if(pID>0){
