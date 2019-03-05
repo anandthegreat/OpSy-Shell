6c55a10ade38676db474208fdb7d94b630758a36
amanrehman <aman17278@iiitd.ac.in>
2019-03-05 14:40:52 +0530

    cd and 2>&1

--- shell.c
+++ shell.c
@@ -8,7 +8,7 @@
 #include<signal.h>
 
 enum type {
-  OVERWRITE2FILE, APPEND2FILE, READFROMFILE, PIPE, OPRDRCT, ERRDRCT, NORMAL
+  OVERWRITE2FILE, APPEND2FILE, READFROMFILE, PIPE, OPRDRCT, ERRDRCT, ERRTOFD, CHDIR, NORMAL
   //>>            >            <             |     1>       2>
 } Type;
 
@@ -22,6 +22,10 @@ void tokeniser(char *input,char **args){
   args[++i]=NULL;
 }
 
+// void changeSTDOUT(){
+//   close(1);
+// }
+
 enum type redirectionChecker(char *input,char **args){
 
   char *p=strstr(input,">>");                               //check if >> exists in input string
@@ -29,16 +33,19 @@ enum type redirectionChecker(char *input,char **args){
   char *r=strstr(input,"<");                                //check if < exists in the input string
   char *s=strstr(input,"|");                                //check if | exists in the input string
   char *outputRedirect=strstr(input,"1>");
-  char *errorRedirect=strstr(input,"2>");
+  char *errorRedirect2File=strstr(input,"2>");
+  char *errorRedirect2FD=strstr(input,"&");
+  char *changeDir=strstr(input,"cd");
 //   int i=0;
 //   args[i] = strtok(input,">");                           //arr[0] will have command name
 //   while(args[i]!=NULL) args[++i] = strtok(NULL,">");
 // printf("%d\n", i);
 
-
-  if(p!=NULL)      return APPEND2FILE; //should be above q!=NULL check
+  if(changeDir!=NULL) return CHDIR;
+  else if(p!=NULL)      return APPEND2FILE; //should be above q!=NULL check
+  else if(errorRedirect2FD!=NULL) return ERRTOFD;
   else if(outputRedirect!=NULL) return OPRDRCT;
-  else if(errorRedirect!=NULL) return ERRDRCT;
+  else if(errorRedirect2File!=NULL) return ERRDRCT;
   else if(q!=NULL) return OVERWRITE2FILE;
   else if(r!=NULL) return READFROMFILE;
   else if(s!=NULL) return PIPE;
@@ -48,7 +55,12 @@ enum type redirectionChecker(char *input,char **args){
 }
 void redirectionExecution(char *input,char *filename,char **args,enum type r_type){ //r_type = redirection type
 
-  if(r_type==APPEND2FILE)             // >> case
+
+  if(r_type==CHDIR){
+
+
+  }
+  else if(r_type==APPEND2FILE)             // >> case
 //  if(filename[0]=='>') //for append not working
   {
     //printf("%s\n",">> case" );
@@ -146,7 +158,6 @@ void redirectionExecution(char *input,char *filename,char **args,enum type r_typ
       }
     }
   }
-
   else if(r_type==ERRDRCT){
     close(2);
     int fd;
@@ -161,6 +172,21 @@ void redirectionExecution(char *input,char *filename,char **args,enum type r_typ
       }
     }
   }
+  else if(r_type==ERRTOFD){
+    // printf("in here \n");
+    close(1);
+    int fd;
+    if(fd = open("error.txt",O_WRONLY|O_CREAT|O_TRUNC,0777)==-1){
+      perror("error: failed to open/create file");
+    }
+    close(2);
+    dup(1);
+    tokeniser(input,args);
+    if(execvp(args[0],args)<0){
+      perror("error: exec failed");
+      exit(1);
+    }
+  }
 }
 
 void commandExecution(char input[])
@@ -171,7 +197,14 @@ void commandExecution(char input[])
       signal(SIGINT, SIG_DFL);
       char *args[20];
       Type=redirectionChecker(input, args);
-      if(Type==NORMAL)
+
+      if(Type==CHDIR){
+        char *dirname;
+        strtok(input," ");
+        dirname= strtok(NULL," ");
+        chdir(dirname);
+      }
+      else if(Type==NORMAL)
       {
         tokeniser(input,args);
         if(execvp(args[0],args)<0){                   //execute command and check if unsuccessful
@@ -215,7 +248,7 @@ void commandExecution(char input[])
         strtok(input,"1>");
         // printf("%s190\n", input);
         filename= strtok(NULL,"1>");       //filename will be command after 1> in this case
-        // printf("%s192\n", args);
+        // printf("%s192\n", filename);
         trimLeadingSpaces(filename,' ');
         redirectionExecution(input,filename,args,OPRDRCT);
       }
@@ -226,6 +259,14 @@ void commandExecution(char input[])
         trimLeadingSpaces(filename,' ');
         redirectionExecution(input,filename,args,ERRDRCT);
       }
+      else if(Type==ERRTOFD){
+        char *filename;
+        strtok(input,"2>");
+        filename= strtok(NULL,"2>");       //filename will be command after pipe in this case
+        trimLeadingSpaces(filename,' ');
+        redirectionExecution(input,filename,args,ERRTOFD);
+      }
+
     }
     //****************parent process(OpSy Shell)******************
     else if(pID>0){
